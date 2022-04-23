4b4f16c5e63cc92850f6413a4737980462e91753
priyanshsingh <priyanshsingh1401@gmail.com>
2022-04-23 16:51:46 +0530

    added material ui init files

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Typography } from '@material-ui/core'; 
+
+export default function App() {
+  return (
+    <div>
+        <Typography variant='h1'>Hello, World!</Typography>
+    </div>
+  )
+}
