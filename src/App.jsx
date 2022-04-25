91023353a6cf08f63ef57d74f5c57607adde2dcb
priyanshsingh <priyanshsingh1401@gmail.com>
2022-04-25 10:03:19 +0530

    added app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
-import { Typography } from '@material-ui/core'; 
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, Button, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
+import { PhotoCamera } from '@material-ui/icons'
 
 export default function App() {
   return (
-    <div>
-        <Typography variant='h1'>Hello, World!</Typography>
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position='relative'>
+        <Toolbar>
+          <PhotoCamera />
+          <Typography variant='h6'>Photo Gallery</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div>
+          <Container maxWidth="sm">
+            <Typography variant='h2' align='center' color='textPrimary' gutterBottom>Galleria</Typography>
+            <Typography variant='h5' align='center' color='textSecondary' paragraph>
+              Hello everyone, this is a photo album !
+            </Typography>
+            <div>
+              <Grid container spacing={2} justify='center'>
+                <Grid item>
+                  <Button variant='contained' color='Primary'>
+                    See my photos
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant='outlined' color='Primary'>
+                    See pictures
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+      </main>
+    </>
   )
 }
+
+
